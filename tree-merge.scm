b27b4730aba5e32a173946e888c003e7aaff0c33
Bogdan Buduroiu <bbuduroiu@gmail.com>
2016-12-01 13:56:12 +0000

    Tree merge now uses set-union

--- tree-merge.scm
+++ tree-merge.scm
@@ -2,7 +2,7 @@
 (define tree-merge
   (lambda (t1 t2)
     (cond ((leaf? t1)
-	    (if (leaf? t2) (make-leaf (append (tree-labels t1) (tree-labels t2)))
-			   (make-node (append (tree-labels t1) (tree-labels t2)) (node-left t2) (node-right t2))))
-	  ((leaf? t2) (make-node (append (tree-labels t1) (tree-labels t2)) (node-left t1) (node-right t1)))
-	  (else (make-node (append (tree-labels t1) (tree-labels t2)) (tree-merge (node-left t1) (node-left t2)) (tree-merge (node-right t1) (node-right t2)))))))
+	    (if (leaf? t2) (make-leaf (set-union (tree-labels t1) (tree-labels t2)))
+			   (make-node (set-union (tree-labels t1) (tree-labels t2)) (node-left t2) (node-right t2))))
+	  ((leaf? t2) (make-node (set-union (tree-labels t1) (tree-labels t2)) (node-left t1) (node-right t1)))
+	  (else (make-node (set-union (tree-labels t1) (tree-labels t2)) (tree-merge (node-left t1) (node-left t2)) (tree-merge (node-right t1) (node-right t2)))))))
